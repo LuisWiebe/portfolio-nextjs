0a196995daed645492758b7917435f7dbddb9903
Chelsea Vick <wiebe.shayne@gmail.com>
2026-04-29 11:39:03 -0700

    adding some stuff

--- nextjs/app/page.jsx
+++ nextjs/app/page.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import projects from "@/app/data/projects";
 // import Navigation from "@/components/header/Navigation";
 // import Footer from "@/components/footer/Footer";
 import LinkSWS from "@/assets/Link-SWS.webp";
@@ -9,6 +11,24 @@ import HowlPhoto from "@/assets/Howl-Photo.jpg";
 import Landscape from "@/assets/GDES131_IllustrativeSet_FA25_Landscape_LWiebe.png";
 import Collab from "@/assets/collab.jpg";
 import Sunlight from "@/assets/Sunlight.jpg";
+
+export default function ProjectsPage () {
+    return (
+        <main>
+            <h1>Projects</h1>
+
+            {projects.map((project) => (
+                <article key={project.slug}>
+                    <h2>{project.title}</h2>
+                    <p>{project.description}</p>
+                    <Link href={`/projects/${project.slug}`}>View Project
+                        </Link>
+                        
+                </article>
+            ))}
+        </main>
+    )
+}
 
 
 
---
+++ nextjs/app/projects/page.js
@@ -0,0 +1,14 @@
+import projects from "@/app/data/projects";
+
+export default function ProjectPage ({ params }) {
+  const project = projects.find((project) => project.slug === params.slug);
+  if (!project) {
+    return <h1>Project not found</h1>
+  }
+  return (
+    <main>
+        <h1>{project.title}</h1>
+        <p> {project.description}</p>
+    </main>
+  )
+}
