5ef12a6d9857ee43ac4dba16184fb57bb41844d2
Chelsea Vick <wiebe.shayne@gmail.com>
2026-05-06 11:35:10 -0700

    Update page.jsx

--- nextjs/app/page.jsx
+++ nextjs/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import styles from "../page.module.css";
 import Link from "next/link";
 import projects from "@/app/data/projects";
 // import Navigation from "@/components/header/Navigation";
